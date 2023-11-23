1d56ec4a78689dd7fe533b65722c8819d4bd5c4c
CyprianESPI <cyprianespinoza@gmail.com>
2023-11-23 16:25:57 +0100

    Set formats to handle 1D barcode. npm i @zxing/library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ZXingScannerModule } from '@zxing/ngx-scanner'
+import { BarcodeFormat } from '@zxing/library'
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,9 @@ import { ZXingScannerModule } from '@zxing/ngx-scanner'
 })
 export class AppComponent {
   title = 'wwang';
+  public AllowedFormats = [BarcodeFormat.QR_CODE, BarcodeFormat.EAN_13, BarcodeFormat.CODE_128, BarcodeFormat.DATA_MATRIX /*, ...*/];
+
+  SuccessScan(result: string): void {
+    console.log(result);
+  }
 }
