933775f92c5e0c678c5cc6e261473f303ef32fd9
CyprianESPI <cyprianespinoza@gmail.com>
2023-11-23 16:05:00 +0100

    Import ZXingScannerModule from npm i @zxing/ngx-scanner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ZXingScannerModule } from '@zxing/ngx-scanner'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ZXingScannerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
