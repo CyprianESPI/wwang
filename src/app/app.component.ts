3f9b92cec05f57a6a6b0f439f5c4cd17cefae076
CyprianESPI <cyprianespinoza@gmail.com>
2023-11-23 17:57:03 +0100

    Improve UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { ZXingScannerModule } from '@zxing/ngx-scanner'
-import { BarcodeFormat } from '@zxing/library'
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import { BarcodeFormat } from '@zxing/library';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ZXingScannerModule],
+  imports: [CommonModule, RouterOutlet, ZXingScannerModule,
+    MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatIconModule,
+    MatToolbarModule, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
