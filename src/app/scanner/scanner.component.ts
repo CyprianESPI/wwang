ade81025b96146e2c3c2ca37a1080149f12128ff
CyprianESPI <cyprianespinoza@gmail.com>
2023-11-26 10:09:50 +0100

    ng generate component scanner

---
+++ src/app/scanner/scanner.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-scanner',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './scanner.component.html',
+  styleUrl: './scanner.component.css'
+})
+export class ScannerComponent {
+
+}
