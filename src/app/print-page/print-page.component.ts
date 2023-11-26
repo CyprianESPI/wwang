6615674f7f7d09ed5c025769e46ecdeed616460e
CyprianESPI <cyprianespinoza@gmail.com>
2023-11-26 15:54:06 +0100

    Fix flex wrap

--- src/app/print-page/print-page.component.ts
+++ src/app/print-page/print-page.component.ts
@@ -12,17 +12,4 @@ import { Item } from '../db/db';
 })
 export class PrintPageComponent {
   @Input() items!: Item[];
-  item = {
-    "Num": "A0001",
-    "Name": "",
-    "Name_EN": "Hata ramune",
-    "Barcode": "4902494008004",
-    "Price": "2.00",
-    "Stock": "60.0",
-    "Price1": "1.09",
-    "Discount": "0.0",
-    "TVA": "1",
-    "Field10": "",
-    "Field11": ""
-  };
 }
